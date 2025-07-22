07fd28e45acbfc2d7d48dfda79ff0a34922e3dc1
Abdu Jemal <abdu1994jemal@gmail.com>
2025-07-22 03:05:08 +0300

    audio ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { useEffect, useRef, useState } from "react";
 import { CourseType } from "../../types";
 import { finished } from "stream";
 import { toast } from "react-toast";
-
+import AudioPlayer from "react-h5-audio-player";
+import 'react-h5-audio-player/lib/styles.css'; 
 
 export default function Home() {
   const [course, setCourse] = useState<CourseType | null>(null); // Using 'any' for simplicity, replace with CourseType if defined
@@ -89,7 +90,38 @@ export default function Home() {
       <p className="text-3xl"><strong>{course.title}</strong></p>
       <p className="text-2xl">{course.ustaz}</p>
       <p className="text-2xl">No of audio {course.courseIds.split(",").length}</p>
-      <audio controls src={getLastAudioUrl()} />
+      <div className="w-full shadow-md bg-bg1-light dark:bg-bg1-dark ">
+
+      <div className=" max-w-screen-2xl m-auto z-50 bg-bg1-light dark:bg-bg1-dark ">
+        {/* Time display */}
+        {/* <div className="flex justify-between text-xs px-4 pt-2 text-gray-600 dark:text-gray-300">
+          <span>{formatTime(currentTime)}</span>
+          <span>{formatTime(duration)}</span>
+        </div> */}
+
+        {/* Audio player */}
+        <AudioPlayer
+            src={getLastAudioUrl()}
+            autoPlay
+            showSkipControls
+            onPlay={() => {}}
+            onPause={() => {}}
+            onClickNext={()=> {}}
+            onClickPrevious={()=> {}}
+            onEnded={()=>{}}
+            header={`${course.title} በ${course.ustaz}`}
+            style={
+              {
+                border: "0",
+                boxShadow: "none",
+                color: "inherit"
+              }
+            }
+            className="rounded-none border-none shadow-none m-auto"
+          />
+      </div>
+    </div>
+      {/* <audio controls src={getLastAudioUrl()} /> */}
       <a className="pt-2 pb-2 pr-4 pl-4 cursor-pointer bg-blue-600 rounded-2xl" target="_blank" href={course.pdfId.split(",")[course.pdfId.split(",").length - 1]}>Show the pdf</a>
       <div className="flex items-center gap-5 pt-6">
         <button onClick={()=>saveToSupa(true)} className="pt-2 pb-2 pr-4 pl-4 cursor-pointer bg-blue-600 rounded-2xl"> { loading ? "Loading..." : "Finished" }</button>
