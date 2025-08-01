aead56174298f9838e38c9de51fbfb376bd2daf8
Abdu Jemal <abdu1994jemal@gmail.com>
2025-08-01 17:38:42 +0300

    10 sec and non pdf  just pass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
   const [course, setCourse] = useState<CourseType | null>(null); // Using 'any' for simplicity, replace with CourseType if defined
   const [loading, setLoading] = useState<boolean>(false);
   const [no, setNo] = useState<number | null>(null);
+  const audioRef = useRef<AudioPlayer | null>(null)
 
   const serverUrl = "https://www.ilmfelagi.com/api/v1/courses/byNumber"
 
@@ -79,6 +80,17 @@ export default function Home() {
     refreshCourse()
   }
 
+  const _10sec = (forward : Boolean) => {
+    const audio = audioRef.current?.audio?.current;
+    if (audio) {
+      if(forward){
+        audio.currentTime = Math.min(audio.currentTime + 10, audio.duration);
+      }else{
+        audio.currentTime = Math.min(audio.currentTime - 10, audio.duration);
+      }
+    }
+  };
+
   if(course === null || no === null) {
     return (
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -103,13 +115,12 @@ export default function Home() {
 
         {/* Audio player */}
         <AudioPlayer
+            ref={audioRef}
             src={getLastAudioUrl()}
             autoPlay
             showSkipControls
-            onPlay={() => {}}
-            onPause={() => {}}
-            onClickNext={()=> {}}
-            onClickPrevious={()=> {}}
+            onClickNext={()=> _10sec(true)}
+            onClickPrevious={()=> _10sec(false)}
             onEnded={()=>{}}
             header={`${course.title} በ${course.ustaz}`}
             style={
@@ -123,12 +134,19 @@ export default function Home() {
           />
       </div>
     </div>
-      {/* <audio controls src={getLastAudioUrl()} /> */}
-      <a className="pt-2 pb-2 pr-4 pl-4 cursor-pointer bg-blue-600 rounded-2xl" target="_blank" href={course.pdfId.split(",")[course.pdfId.split(",").length - 1]}>Show the pdf</a>
-      <div className="flex items-center gap-5 pt-6">
-        <button onClick={()=>saveToSupa(true)} className="pt-2 pb-2 pr-4 pl-4 cursor-pointer bg-blue-600 rounded-2xl"> { loading ? "Loading..." : "Finished" }</button>
-        <button onClick={()=>saveToSupa(false)} className="pt-2 pb-2 pr-4 pl-4  cursor-pointer bg-blue-600 rounded-2xl">{ loading ? "Loading..." : "Not Finished" }</button>
+      {
+        course.pdfId && course.pdfId.trim().length > 0 ?
+      <div className="flex flex-col items-center gap-3 pt-6">
+        <a className="pt-2 pb-2 pr-4 pl-4 cursor-pointer bg-blue-600 rounded-2xl" target="_blank" href={course.pdfId.split(",")[course.pdfId.split(",").length - 1]}>Show the pdf</a>
+        <div className="flex items-center gap-5 pt-6">
+          <button onClick={()=>saveToSupa(true)} className="pt-2 pb-2 pr-4 pl-4 cursor-pointer bg-blue-600 rounded-2xl"> { loading ? "Loading..." : "Finished" }</button>
+          <button onClick={()=>saveToSupa(false)} className="pt-2 pb-2 pr-4 pl-4  cursor-pointer bg-blue-600 rounded-2xl">{ loading ? "Loading..." : "Not Finished" }</button>
+        </div>
       </div>
+      :
+      <button onClick={()=>saveToSupa(true)} className="pt-2 pb-2 pr-4 pl-4 cursor-pointer bg-blue-600 rounded-2xl"> { loading ? "Loading..." : "Continue" }</button>
+      }
+      
     </div>
   );
 }
